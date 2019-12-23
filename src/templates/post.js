2aa42ed2bdf3e81b1a2da25a2d557caeef8ab7c0
Waseem Ahmed <codewaseem@gmail.com>
2019-12-23 15:39:09 +0530

    use query to dynamically populate data

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,19 +3,32 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ReadLink from '../components/read-link';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
-const PostTemplate = () => {
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        author
+        title
+      }
+      body
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => {
   return (
     <Layout>
-      <h1>post title</h1>
+      <h1>{post.frontmatter.title}</h1>
       <p
         css={css`
           font-size: 0.75rem;
         `}
       >
-        Posted by (author)
+        Posted by {post.frontmatter.author}
       </p>
-      <p>post body goes here</p>
+      <MDXRenderer>{post.body}</MDXRenderer>
       <ReadLink to="/">Back to all posts</ReadLink>
     </Layout>
   );
