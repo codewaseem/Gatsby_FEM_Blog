dabe989241d6ffbc5d0ef2518b87e8a2b70580c1
Waseem Ahmed <codewaseem@gmail.com>
2019-12-25 21:24:39 +0530

    use instagram source

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 import Hero from '../components/hero';
+import Insta from '../components/insta';
 
 export default () => {
   const posts = usePosts();
@@ -15,6 +15,7 @@ export default () => {
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
